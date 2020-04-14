4eca9897a4fbc09186b03440cb3a1e73b5770ac6
heronguyen2607 <heronguyen2607@gmail.com>
2020-04-15 01:12:48 +0700

    Updates

---
+++ precache-manifest.dcf85922f089f6522fbb91fb3737c5dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f97c02dcf37e3b34b8b7f21507c1391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe96ca470f5b2273c7e1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0568a3aa3fc6fafdc93a",
+    "url": "/static/js/2.b15206df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b15206df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe96ca470f5b2273c7e1",
+    "url": "/static/js/main.06a1147a.chunk.js"
+  },
+  {
+    "revision": "356b394eaf11e87eeab2",
+    "url": "/static/js/runtime-main.bd62ba74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
